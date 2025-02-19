bdeeb94d969ca95416c1918eb60682c89f8eccc0
joshuasilva414 <joshuasilva414@gmail.com>
2025-02-19 12:49:34 -0600

    Add hero title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen night flex flex-col justify-end">
+    <div className="w-full h-screen relative night flex flex-col justify-end">
       <div className="w-screen">
         <Image
           className="w-full"
@@ -13,6 +13,14 @@ export default function Home() {
           unoptimized={true}
         ></Image>
       </div>
+      <Image
+        className="w-full absolute left-0 top-1/2 -translate-y-2/3"
+        src={"/rh-xi-hero-title.svg"}
+        alt={"Rh Xi Hero Title"}
+        width={1000}
+        height={1000}
+        unoptimized={true}
+      ></Image>
     </div>
   );
 }
